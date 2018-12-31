3fc076732e0672cc11d44b108e3c3cf14c227bd2
Saurabh Dhingra <saudhing@publicisgroupe.net>
2018-12-31 17:54:46 +0530

    third commit - extent report

--- src/main/java/com/saudhing/utilities/ExtentManager.java
+++ src/main/java/com/saudhing/utilities/ExtentManager.java
@@ -1,6 +1,7 @@
 package com.saudhing.utilities;
 
 import java.io.File;
+import java.util.Date;
 
 import com.relevantcodes.extentreports.DisplayOrder;
 import com.relevantcodes.extentreports.ExtentReports;
@@ -14,9 +15,14 @@ public class ExtentManager {
 		
 		if(extent==null){
 			
+			Date d = new Date();
+			String fileName = "jenkins_" + d.toString().replace(":", "_").replace(" ", "_")+ ".html";
 			
-			extent = new ExtentReports(System.getProperty("user.dir")+"\\target\\surefire-reports\\html\\extent.html",true,DisplayOrder.OLDEST_FIRST);
+			extent = new ExtentReports(System.getProperty("user.dir")+"\\reports\\"+fileName,true,DisplayOrder.OLDEST_FIRST);
 			extent.loadConfig(new File(System.getProperty("user.dir")+"\\src\\test\\resources\\extentconfig\\ReportsConfig.xml"));
+
+			/*extent = new ExtentReports(System.getProperty("user.dir")+"\\target\\surefire-reports\\html\\extent.html",true,DisplayOrder.OLDEST_FIRST);
+			extent.loadConfig(new File(System.getProperty("user.dir")+"\\src\\test\\resources\\extentconfig\\ReportsConfig.xml"));*/
 			
 		}
 		
