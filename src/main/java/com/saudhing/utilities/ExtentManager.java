fef74978783f9b7d3e76ceef97e4eebdd482eac9
Saurabh Dhingra <saudhing@publicisgroupe.net>
2018-12-31 19:17:01 +0530

    seventh commit - revert Extent Report path

--- src/main/java/com/saudhing/utilities/ExtentManager.java
+++ src/main/java/com/saudhing/utilities/ExtentManager.java
@@ -15,15 +15,15 @@ public class ExtentManager {
 		
 		if(extent==null){
 			
-			Date d = new Date();
+			/*Date d = new Date();
 			String fileName = "jenkins_" + d.toString().replace(":", "_").replace(" ", "_")+ ".html";
 			
 			extent = new ExtentReports(System.getProperty("user.dir")+"\\reports\\"+fileName,true,DisplayOrder.OLDEST_FIRST);
-			extent.loadConfig(new File(System.getProperty("user.dir")+"\\src\\test\\resources\\extentconfig\\ReportsConfig.xml"));
+			extent.loadConfig(new File(System.getProperty("user.dir")+"\\src\\test\\resources\\extentconfig\\ReportsConfig.xml"));*/
 
 			
-			/*extent = new ExtentReports(System.getProperty("user.dir")+"\\target\\surefire-reports\\html\\extent.html",true,DisplayOrder.OLDEST_FIRST);
-			extent.loadConfig(new File(System.getProperty("user.dir")+"\\src\\test\\resources\\extentconfig\\ReportsConfig.xml"));*/
+			extent = new ExtentReports(System.getProperty("user.dir")+"\\target\\surefire-reports\\html\\extent.html",true,DisplayOrder.OLDEST_FIRST);
+			extent.loadConfig(new File(System.getProperty("user.dir")+"\\src\\test\\resources\\extentconfig\\ReportsConfig.xml"));
 			
 		}
 		
